e22c40d33e7c0cec60a5c22a6cc29e491c74c6eb
cccpxd <cristiancolorado8.5@gmail.com>
2025-10-03 14:59:08 -0500

    INTENTO HACER EL LOGIN

--- Frontend/server.js
+++ Frontend/server.js
@@ -56,8 +56,7 @@ app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
 
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "Frontend/users.js";
-
+const User = require("./user.js");
 const JWT_SECRET = process.env.MONGOTOKEN; // cámbialo por uno seguro
 
 // Ruta login
